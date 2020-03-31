b23aa9686ffaddcfc697af845ecf8eca490d2b60
Dwayne <driv91197@gmail.com>
2020-03-31 14:09:33 -0400

    Home page working, added new components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import home from './components/home';
+import about from './components/about'
+import blogs from './components/blogs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={home} />
+          <Route path='/about' component={about} />
+          <Route path='/blogs' component={blogs} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Jumbotron, Button } from 'react-bootstrap';
+import './home.css';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
-        )
+            <Container>
+                <Jumbotron>
+                    <h2>Welcome to Pheebs Blog!</h2>
+                    <p>Random text to make sure everything is working</p>
+                </Jumbotron>
+                <Link to='\about'>
+                    <Button bsStyle='primary'>About</Button>
+                </Link>
+            </Container>
+        );
     }
 }
