3d61ac353a975526ce6c2f129280e24abfe0a13c
Dwayne <driv91197@gmail.com>
2020-05-17 17:01:33 -0400

    Cleaned up code

--- src/components/customFooter.jsx
+++ src/components/customFooter.jsx
@@ -11,8 +11,16 @@ export default class Footer extends Component {
             <Navbar className='footer'>
                 <Nav.Link to='/' className='contact'>Contact</Nav.Link>
                 <div className='icons'>
-                    <Nav.Link href='https://instagram.com/phoebekarin?igshid=15kx0e67w97w' target='_blank'><FontAwesomeIcon className='insta' icon={faInstagram} size='lg'></FontAwesomeIcon></Nav.Link>
-                    <Nav.Link href='https://twitter.com/Phoebekarin' target='_blank'><FontAwesomeIcon className='twitter' icon={faTwitter} size='lg'></FontAwesomeIcon></Nav.Link>
+                    <Nav.Link 
+                        href='https://instagram.com/phoebekarin?igshid=15kx0e67w97w' 
+                        target='_blank'>
+                            <FontAwesomeIcon className='insta' icon={faInstagram} size='lg'></FontAwesomeIcon>
+                    </Nav.Link>
+                    <Nav.Link 
+                        href='https://twitter.com/Phoebekarin' 
+                        target='_blank'>
+                            <FontAwesomeIcon className='twitter' icon={faTwitter} size='lg'></FontAwesomeIcon>
+                    </Nav.Link>
                 </div>
             </Navbar>
         )
